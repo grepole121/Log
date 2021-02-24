62009e620a83195398096f14d93576024837e375
George Repole <grepole121@gmail.com>
2021-02-24 02:19:25 +0000

    User can now create the key if it does not already exist. Then user inputs what they want to append to the log file. The program then decrypts the log file, appends what the user inputted, then encrypts the file again. The log file is now encrypted. Calling the decrypt function will decrypt the file. Since the file can be decrypted as long as someone has the key this may be changed so the password is required for every edit and the key is never stored in a file.

--- log.py
+++ log.py
@@ -1,14 +1,39 @@
-#import time
-import pass_key_gen
+import time, os, os.path, getpass
+from pass_key_gen import gen_key
+from cryptography.fernet import Fernet
+
+if not os.path.isfile('key.key'):
+    #Create a key with a user password if the key doesn't exist
+    gen_key(getpass.getpass("Please enter your password: "))
+
+f = Fernet(open("key.key", "rb").read())
+file_destination = "/home/george/MEGA/Log/New/log.txt"
 
 
+def encrypt():
+    #Encrypt the file after changes have been made
+    with open(file_destination, "rb") as log:
+        log_data = log.read()
+    encrypted_log = f.encrypt(log_data)
+    with open(file_destination, "wb") as log:
+        log.write(encrypted_log)
 
 
+def decrypt():
+    #Decrypt the file so changes can be written
+    if os.path.isfile(file_destination):
+        with open(file_destination, "rb") as log:
+            encrypted_data = log.read()
+        decrypted_data = f.decrypt(encrypted_data)
+        with open(file_destination, "wb") as log:
+            log.write(decrypted_data)
 
 
-#date = time.strftime('%d/%m/%Y')
-#time = time.strftime('%H:%M:%S')
-#print('What would you like to append')
-#log_event = input()
-#with open('/home/george/MEGA/Log/New/log.txt', 'a') as myfile:
-    #myfile.write('[ ' + date + ' - ' + time + ' ] - ' + log_event + '\n')
+date = time.strftime('%d/%m/%Y')
+time = time.strftime('%H:%M:%S')
+print('What would you like to append')
+log_input = input()
+decrypt()
+with open(file_destination, 'a') as myfile:
+    myfile.write('[ ' + date + ' - ' + time + ' ] - ' + log_input + '\n')
+encrypt()
--- pass_key_gen.py
+++ pass_key_gen.py
@@ -2,20 +2,18 @@ import os, base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
-from cryptography.fernet import Fernet
 
 backend = default_backend()
-salt = os.urandom(16)
-passw = input()
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=100000,
-    backend=backend
-)
-key = base64.urlsafe_b64encode(kdf.derive(passw.encode()))
+salt = b'\xd5z\xe49\xca\xd0\xa8\xd6\xb0\x0e\x1c\xc9\x80\xb2t2'
 
-with open("key.key", "wb") as key_file:
-    key_file.write(key)
-
+def gen_key(passw):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=backend
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(passw.encode()))
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
