c24dec79e2b0c3aba2ac94444eeb9969a288ab90
George Repole <grepole121@gmail.com>
2021-02-25 02:48:02 +0000

    Done much more with the GUI. Can now decrypt, read, and encrypt the file in the gui. Need to create the code for adding to the file from the GUI and to create the key with a password in the GUI.

--- gui_testing.py
+++ gui_testing.py
@@ -1,25 +1,102 @@
 import sys
-
-# 1. Import `QApplication` and all the required widgets
 from PyQt5.QtWidgets import *
+from crypt import decrypt, encrypt
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 600, 600)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
+# TODO:
+# Need to create the code for the user to add to the file
+# Create the code for the user creating a password in the GUI if the key doesn't exist
 
-vbox = QVBoxLayout()
+class ReadWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.readlog()
 
-text_edit = QPlainTextEdit()
-text=open('log.txt').read()
-text_edit.setPlainText(text)
+    def initUI(self):
+        self.setWindowTitle('Reading...')
+        self.setGeometry(0,0,500,500)
+        self.off_center()
 
-vbox.addWidget(text_edit)
-window.setLayout(vbox)
+    def off_center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center() * 1.1
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def readlog(self):
+        vbox = QVBoxLayout()
+
+        text_edit = QPlainTextEdit()
+        decrypt()
+        with open("log.txt", "r") as keyfile:
+            text = keyfile.read()
+        encrypt()
+        text_edit.setPlainText(text)
+
+        vbox.addWidget(text_edit)
+        self.setLayout(vbox)
+
+class AddWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
 
-window.show()
-sys.exit(app.exec_())
+    def initUI(self):
+        self.setWindowTitle('Append...')
+        self.setGeometry(0,0,500,500)
+        self.off_center()
+
+    def off_center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center() * 1.1
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.readwindow = ReadWindow()
+        self.addwindow  = AddWindow()
+        self.create_buttons()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Log')
+        self.setGeometry(0,0,500,500)
+        self.center()
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def open_read_window(self):
+        self.readwindow.show()
+
+    def create_buttons(self):
+        layout = QHBoxLayout()
+
+        readbtn = QPushButton('Read')
+        readbtn.clicked.connect(lambda: self.open_read_window())
+        layout.addWidget(readbtn)
+
+        addbtn = QPushButton('Add to Log')
+        addbtn.clicked.connect(lambda: self.open_add_window())
+        layout.addWidget(addbtn)
+
+        self.setLayout(layout)
+
+    def open_add_window(self):
+        self.addwindow.show()
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    sys.exit(app.exec_())
--- log.py
+++ log.py
@@ -1,32 +1,7 @@
 import time, os, os.path, getpass
-from pass_key_gen import gen_key
-from cryptography.fernet import Fernet
+from crypt import decrypt, encrypt
 
-if not os.path.isfile('key.key'):
-    #Create a key with a user password if the key doesn't exist
-    gen_key(getpass.getpass("Please enter your password: "))
-
-f = Fernet(open("key.key", "rb").read())
 file_destination = "/home/george/MEGA/Log/New/log.txt"
-
-
-def encrypt():
-    #Encrypt the file after changes have been made
-    with open(file_destination, "rb") as log:
-        log_data = log.read()
-    encrypted_log = f.encrypt(log_data)
-    with open(file_destination, "wb") as log:
-        log.write(encrypted_log)
-
-
-def decrypt():
-    #Decrypt the file so changes can be written
-    if os.path.isfile(file_destination):
-        with open(file_destination, "rb") as log:
-            encrypted_data = log.read()
-        decrypted_data = f.decrypt(encrypted_data)
-        with open(file_destination, "wb") as log:
-            log.write(decrypted_data)
 
 
 date = time.strftime('%d/%m/%Y')
